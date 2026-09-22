e4783cb8ca9cad0b2884d9e1e4f32a52da75c8d7
weedogcat <markalexisbatis@gmail.com>
2026-09-22 19:02:49 +0800

    fix: hermes url polyfill for supabase & v1.0.5

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import "react-native-url-polyfill/auto";
 import { Platform } from "react-native";
 import { registerWidgetTaskHandler } from "react-native-android-widget";
 import { widgetTaskHandler } from "./widgets/widget-task-handler";

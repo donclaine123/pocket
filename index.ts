a73c60fbc4b265d806fc1106f912c56f35f831a7
weedogcat <markalexisbatis@gmail.com>
2026-09-22 16:42:22 +0800

    tracking the issue

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
-import { Platform } from "react-native";
 import { registerWidgetTaskHandler } from "react-native-android-widget";
 import { widgetTaskHandler } from "./widgets/widget-task-handler";
 import "expo-router/entry";
 
-// Register the Android background widget task handler only on Android
-if (Platform.OS === "android") {
-  registerWidgetTaskHandler(widgetTaskHandler);
-}
-
+// Register the Android background widget task handler
+registerWidgetTaskHandler(widgetTaskHandler);
